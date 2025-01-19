2bcfdf87247f6bcfb8672062c119357651c89aa2
pirateno <160731944+pirateno@users.noreply.github.com>
2025-01-19 16:32:35 -0300

    Add files via upload

---
+++ 12/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortscurts import render, HttpResponse
+def index(request):
+    return render(request,'index.html')
+
+def sobre(request):
+    return render(request, 'sobre.html')
+
+def contato(request):
+    return render(request, 'contato.html')
+
+def ajuda(request):
+    return render(request, 'ajuda.html')
